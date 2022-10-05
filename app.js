706cded4fc921be82d5e3fd6d779ca63e6d16848
Licia Thörn <liciathorn@gmail.com>
2022-10-05 20:42:56 +0200

    Starting css file+declaring path to folder public

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const middleware = require('./middleware');
+const path = require('path');
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(express.static(path.join(__dirname, "public")))
+
 // Routes
 
 const loginRoute = require('./routes/loginRoutes');
+
 app.use("/login", loginRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
