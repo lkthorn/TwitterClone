68a87a8fb30eeee712fb379a9426582a8e214b58
Licia Thörn <liciathorn@gmail.com>
2022-10-05 15:14:12 +0200

    Rendering content on the webpage

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const server = app.listen(port, () => console.log("Server listening on port " + port));
+
+app.get("/", (req, res, next) => {
+    res.status(200).send("Yes!");
+})
