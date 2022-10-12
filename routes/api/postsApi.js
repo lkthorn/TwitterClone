882893fb676c92face21056828a9b622ade49bee
Licia Thörn <liciathorn@gmail.com>
2022-10-12 14:46:25 +0200

    Create Post Schema

--- routes/api/postsApi.js
+++ routes/api/postsApi.js
@@ -12,6 +12,15 @@ router.get("/", (req, res, next) => {
 })
 
 router.post("/", async (req, res, next) => {
+
+    if(!req.body.content) {
+        console.log("Content param not sent with request");
+        return res.sendStatus(400);
+    }
+
+    
+
+
     res.status(200).send("Worked!");
 })
     
