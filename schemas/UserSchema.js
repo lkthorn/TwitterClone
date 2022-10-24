7862d8388be54b3672514b55c07482f411f2927e
Licia Thörn <liciathorn@gmail.com>
2022-10-24 15:25:54 +0200

    Starting following function

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -46,7 +46,15 @@ const UserSchema = new Schema({
   retweets: [{
     type: Schema.Types.ObjectId,
     ref: 'Post'
-  }]   
+  }], 
+  following: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],     
+  followers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],     
 }, { timestamps: true });
 
 var User = mongoose.model('User', UserSchema);
