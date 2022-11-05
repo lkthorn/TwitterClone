30bd7743af6ffab412a73689db7f2b8474ff8da6
Licia Thörn <liciathorn@gmail.com>
2022-11-05 09:34:08 +0100

    Implementing search function for users

--- public/js/search.js
+++ public/js/search.js
@@ -23,7 +23,15 @@ function search(searchTerm, searchType) {
     var url = searchType == "users" ? "/api/users" : "/api/posts"
 
     $.get(url, { search: searchTerm }, (results) => {
-        console.log(results);
+        
+        if(searchType == "users") {
+            console.log(results);
+
+        }
+
+        else {
+            outputPosts(results, $(".resultsContainer"))
+        }
     })
 
 }
