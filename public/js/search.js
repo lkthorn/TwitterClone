c8d5b77ecffc0d61d6661e42aca80cc4f0bd07ac
Licia Thörn <liciathorn@gmail.com>
2022-11-05 09:41:23 +0100

    Showing users list on search tab

--- public/js/search.js
+++ public/js/search.js
@@ -25,8 +25,7 @@ function search(searchTerm, searchType) {
     $.get(url, { search: searchTerm }, (results) => {
         
         if(searchType == "users") {
-            console.log(results);
-
+            outputUsers(results, $(".resultsContainer"));
         }
 
         else {
